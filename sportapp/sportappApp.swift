1626dd584a34cdf0b3099e56e3b5cab363bf2345
nooreealotibi <nooreealotibi@gmail.com>
2025-04-08 04:41:25 +0300

    Initial Commit

---
+++ sportapp/sportappApp.swift
@@ -0,0 +1,17 @@
+//
+//  sportappApp.swift
+//  sportapp
+//
+//  Created by noor alotibi on 10/10/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct sportappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sportappTests/sportappTests.swift
@@ -0,0 +1,17 @@
+//
+//  sportappTests.swift
+//  sportappTests
+//
+//  Created by noor alotibi on 10/10/1446 AH.
+//
+
+import Testing
+@testable import sportapp
+
+struct sportappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
